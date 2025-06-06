a9d35dc29c86074ef53b5a8cecade6ce6c3ffdd8
werdeengod <azverev587@gmail.com>
2025-06-06 16:20:26 +0400

    Initial commit

--- examples/test.py
+++ examples/test.py
@@ -1,27 +1,20 @@
-import asyncio
 import datetime
-
 from netspoapi import NetSPOApi
 
 
 async def from_context():
-    url = 'http://spo.cit73.ru'
+    url = 'https://spo.cit73.ru'
 
-    async with NetSPOApi('зверев27', 'd7f0e794', base_url=url) as api:
-        print(datetime.datetime.now())
+    async with NetSPOApi('зверев27', '11111', base_url=url) as api:
         print(await api.student_client.attestation(course_number=3, semester=1))
-        print(datetime.datetime.now())
 
 
 async def default():
-    url = 'http://spo.cit73.ru'
+    url = 'https://spo.cit73.ru'
 
-    api = await NetSPOApi('зверев27', 'd7f0e794', base_url=url).login()
-
-    print(datetime.datetime.now())
-    print(await api.student_client.lessons(begin=datetime.date(2025, 1, 1), end=datetime.date(2025, 4, 13)))
-    print(datetime.datetime.now())
-
-
-asyncio.run(from_context())
+    api = await NetSPOApi('зверев27', '11111', base_url=url).login()
+    print(await api.student_client.lessons(
+        begin=datetime.date(2025, 1, 1),
+        end=datetime.date(2025, 4, 13)
+    ))
 
